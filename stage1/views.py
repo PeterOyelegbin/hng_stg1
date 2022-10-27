fade8d9bb13528e2f4c13a46b5a13dcf56d8d278
PeterOyelegbin <peteroyelegbin@gmail.com>
2022-10-27 23:57:00 +0100

    app setup for deployment

---
+++ stage1/views.py
@@ -0,0 +1,14 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+@api_view(['GET'])
+def index(request):
+    json_res = {
+        "slackUsername": "PeterOyelegbin",
+        "backend": True,
+        "age": 26,
+        "bio": "A HVAC Engineer with over 6 years professional experience, preparing for full transition into Tech having had 2 years experience in Django web app development."
+    }
+    return Response(json_res, status=status.HTTP_200_OK)
