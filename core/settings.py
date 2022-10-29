478cb10c44f7c6ab4e80e7aac833340703312428
PeterOyelegbin <peteroyelegbin@gmail.com>
2022-10-29 16:02:13 +0100

    adding cors dependency

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3rd party apps
     'rest_framework',
+    'corsheaders',
 
     # Created app
     'stage1',
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
--- stage1/views.py
+++ stage1/views.py
@@ -1,12 +1,14 @@
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
+@csrf_exempt
 @api_view(['GET'])
 def index(request):
     json_res = {
-        "slackUsername": "PeterOyelegbin",
+        "slackUsername": "Peter Oyelegbin",
         "backend": True,
         "age": 26,
         "bio": "A HVAC Engineer with over 6 years professional experience, preparing for full transition into Tech having had 2 years experience in Django web app development."
